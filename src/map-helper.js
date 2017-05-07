4e513ebc3baf8300f6d634eeb0fb721c1702709d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-05-07 10:40:13 +0200

    map helper doouble function with es5 for loop example

--- src/map-helper.js
+++ src/map-helper.js
@@ -4,6 +4,24 @@ console.group('Map Helper');
 const numbers = [1,2,3];
 const doubledNumbers = [];
 
+// ES5 
 for (let i = 0; i < numbers.length; i++) {
     doubledNumbers.push(numbers[i] + 2);
 }
+
+// ES5
+doubledNumbers;
+console.log('ES5',doubledNumbers)
+
+// E6
+// Return a new array, don't mutate the original array in large applications
+const doubled = numbers.map(function(number) {
+    return number * 2;
+});
+console.log('ES6', doubled);
+
+// for each element in the numbers array we pass each number into 
+// the iterator function, each value gets doubled and added to the 
+// new array.
+
+console.groupEnd();
