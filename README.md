2853afc7e6fa99f6f37c183001a57f00c1a2adad
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-05-07 10:31:01 +0200

    added README

---
+++ README.md
@@ -0,0 +1,7 @@
+#### Playing with ES 6 
+Read the code and launch the local webpack dev server.
+
+```
+$ yarn install
+$ npm run dev
+``
---
+++ src/map-helper.js
@@ -0,0 +1,9 @@
+
+console.group('Map Helper');
+
+const numbers = [1,2,3];
+const doubledNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    doubledNumbers.push(numbers[i] + 2);
+}
